de878b1f9c51140df90489d14f8dea224105958b
Ashley Baldock <ashley@baldock.me>
2018-01-29 10:15:33 +0000

    Create initial project structure

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace search_and_compare_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
