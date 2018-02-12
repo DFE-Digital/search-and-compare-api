eeac00193fedc6c36de6bd7159eeb2c2d5acaedf
Ashley Baldock <ashley.baldock@digital.education.gov.uk>
2018-02-12 16:04:35 +0000

    Fix up namespace

--- src/Startup.cs
+++ src/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GovUk.Education.SearchAndCompareApi
+namespace GovUk.Education.SearchAndCompare.API
 {
     public class Startup
     {
