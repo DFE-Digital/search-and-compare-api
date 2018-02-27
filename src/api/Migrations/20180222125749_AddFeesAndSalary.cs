fb62aacf102bfc9dbc7af83ef96166c323c47448
Ashley Baldock <ashley.baldock@digital.education.gov.uk>
2018-02-27 15:24:58 +0000

    Really fix

--- src/api/Migrations/20180222125749_AddFeesAndSalary.cs
+++ src/api/Migrations/20180222125749_AddFeesAndSalary.cs
@@ -61,7 +61,9 @@ namespace SearchAndCompare.Migrations
                 nullable: false,
                 defaultValue: 9250);
 
-            migrationBuilder.Sql(@"DROP FUNCTION course_distance;");
+            migrationBuilder.Sql(@"DROP FUNCTION course_distance(double precision,
+                                                                 double precision,
+                                                                 double precision);");
             
             migrationBuilder.Sql(@"
                 CREATE OR REPLACE FUNCTION course_distance( lat DOUBLE PRECISION, 
@@ -139,7 +141,9 @@ namespace SearchAndCompare.Migrations
                 name: "feecaps",
                 newName: "fees");
 
-            migrationBuilder.Sql(@"DROP FUNCTION course_distance;");
+            migrationBuilder.Sql(@"DROP FUNCTION course_distance(double precision,
+                                                                 double precision,
+                                                                 double precision);");
 
             migrationBuilder.Sql(@"
                 CREATE OR REPLACE FUNCTION course_distance( lat DOUBLE PRECISION, 
