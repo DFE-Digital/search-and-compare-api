f8d2abba94b39ffc3e666b2b3a9edc30d64634d8
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-21 13:33:03 +0100

    infrastructure: add app insights

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -19,6 +19,7 @@ namespace GovUk.Education.SearchAndCompare.Api
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
