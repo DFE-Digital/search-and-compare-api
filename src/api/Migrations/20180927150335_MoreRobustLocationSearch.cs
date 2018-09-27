a666892f9a5199555ed4ad50a84c4078641e78e4
the-daniel-rothig <daniel.rothig@gmail.com>
2018-09-27 16:07:26 +0100

    update course_distance to allow for no campus and  no providerlocation

---
+++ src/api/Migrations/20180927150335_MoreRobustLocationSearch.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SearchAndCompare.Migrations
+{
+    public partial class MoreRobustLocationSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP FUNCTION course_distance(double precision,
+                                                                 double precision,
+                                                                 double precision);");
+
+            migrationBuilder.Sql(@"CREATE OR REPLACE FUNCTION course_distance( lat DOUBLE PRECISION, 
+                                                            lon DOUBLE PRECISION, 
+                                                            rad DOUBLE PRECISION) 
+                                            RETURNS TABLE (""Id"" integer,
+                                                           ""Distance"" double precision) AS $$
+                    SELECT course.""Id"",                        
+                        MIN(LEAST(earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")), 
+			                earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc2.""Latitude"",loc2.""Longitude"")))) AS ""Distance""
+                    FROM ""course""
+                    JOIN location loc ON course.""ProviderLocationId"" = loc.""Id""
+                    LEFT OUTER JOIN campus on campus.""CourseId"" = course.""Id""
+                    LEFT OUTER JOIN location loc2 ON campus.""LocationId"" = loc2.""Id""
+                    WHERE (loc.""Id"" IS NOT NULL OR loc2.""Id"" IS NOT NULL) AND
+                          (""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"") OR
+		                    ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc2.""Latitude"",loc2.""Longitude""))
+                    AND LEAST(earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")), 
+			                earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc2.""Latitude"",loc2.""Longitude""))) <= rad
+			        GROUP BY course.""Id""
+                $$ LANGUAGE SQL;");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP FUNCTION course_distance(double precision,
+                                                                 double precision,
+                                                                 double precision);");
+
+            migrationBuilder.Sql(@"CREATE OR REPLACE FUNCTION course_distance( lat DOUBLE PRECISION, 
+                                                            lon DOUBLE PRECISION, 
+                                                            rad DOUBLE PRECISION) 
+                                            RETURNS TABLE (""Id"" integer,
+                                                           ""Distance"" double precision) AS $$
+                    SELECT course.""Id"",                        
+                        MIN(LEAST(earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")), 
+			                earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc2.""Latitude"",loc2.""Longitude"")))) AS ""Distance""
+                    FROM ""course""
+                    JOIN location loc ON course.""ProviderLocationId"" = loc.""Id""
+                    JOIN campus on campus.""CourseId"" = course.""Id""
+                    JOIN location loc2 ON campus.""LocationId"" = loc2.""Id""
+                    WHERE (""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"") OR
+		                    ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc2.""Latitude"",loc2.""Longitude""))
+                    AND LEAST(earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")), 
+			                earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc2.""Latitude"",loc2.""Longitude""))) <= rad
+			        GROUP BY course.""Id""
+                $$ LANGUAGE SQL;");
+
+        }
+    }
+}
