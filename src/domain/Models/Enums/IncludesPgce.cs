40c778653c46b6189b9dce51fc09ad060bcb8819
the-daniel-rothig <daniel.rothig@gmail.com>
2018-10-01 12:11:27 +0100

    fix typo; delete obsolete tests; add comments

--- src/domain/Models/Enums/IncludesPgce.cs
+++ src/domain/Models/Enums/IncludesPgce.cs
@@ -2,10 +2,17 @@ namespace GovUk.Education.SearchAndCompare.Domain.Models.Enums
 {
     public enum IncludesPgce
     {
+        /* QTS only search category */
         No = 0, // QtsOnly
+
+
+        /* QTS + PGCE search category */
         Yes = 1, // QtsWithPgce
-        QtsWithOpionalPgce = 2,
+        QtsWithOptionalPgce = 2,
         QtsWithPgde = 3,
+        
+
+        /* "Other" search category */
         QtlsOnly = 4,
         QtlsWithPgce = 5,
         QtlsWithPgde = 6,
