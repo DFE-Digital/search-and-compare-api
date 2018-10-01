fdcb386110cd744436ea9b1c1290e2cc64d353cd
the-daniel-rothig <daniel.rothig@gmail.com>
2018-10-01 12:02:27 +0100

    Add new options to IncludesPgce
    IncludesPgce covers all qualifications option

--- src/domain/Models/Enums/IncludesPgce.cs
+++ src/domain/Models/Enums/IncludesPgce.cs
@@ -2,8 +2,12 @@ namespace GovUk.Education.SearchAndCompare.Domain.Models.Enums
 {
     public enum IncludesPgce
     {
-        No = 0,
-        Yes = 1,
-        Optional = 2
+        No = 0, // QtsOnly
+        Yes = 1, // QtsWithPgce
+        QtsWithOpionalPgce = 2,
+        QtsWithPgde = 3,
+        QtlsOnly = 4,
+        QtlsWithPgce = 5,
+        QtlsWithPgde = 6,
     }
 }
