a75e181ef28cfec1b6996b3e2396f3737089a26e
the-daniel-rothig <daniel.rothig@gmail.com>
2018-10-01 18:53:25 +0100

    Fix course query serialisation
    The courses list is a get request so we need to serialise things into
    a query string. However Lists are not supported at the moment.
    
    To fix the fact that consequently, the qualification filter options got
    dropped between API and UI, we use a bitmap instead.

--- src/domain/Filters/Enums/QualificationOption.cs
+++ src/domain/Filters/Enums/QualificationOption.cs
@@ -2,9 +2,10 @@ namespace GovUk.Education.SearchAndCompare.Domain.Filters.Enums
 {
     public enum QualificationOption
     {
-        QtsOnly = 1,
-        PgdePgceWithQts = 2,
+        QtsOnly = 1 << 0,
+        
+        PgdePgceWithQts = 1 << 1,
 
-        Other = 3
+        Other = 1 << 2
     }
 }
