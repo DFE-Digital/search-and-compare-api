10f047b3c360092a2179d96858b219442e33690e
Tim Abell <tim@timwise.co.uk>
2018-10-04 16:00:13 +0100

    CourseSearchService test - check a location based course is returned

--- tests/Unit.Tests/Services/CourseSearchServiceTests.cs
+++ tests/Unit.Tests/Services/CourseSearchServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using GovUk.Education.SearchAndCompare.Api.DatabaseAccess;
 using GovUk.Education.SearchAndCompare.Api.Services;
 using GovUk.Education.SearchAndCompare.Domain.Filters;
+using GovUk.Education.SearchAndCompare.Domain.Filters.Enums;
+using GovUk.Education.SearchAndCompare.Domain.Models;
 using Moq;
 using NUnit.Framework;
 
@@ -14,13 +18,31 @@ namespace GovUk.Education.SearchAndCompare.Api.Tests.Unit.Tests.Services
         public void CheckFiltering()
         {
             var contextMock = new Mock<ICourseDbContext>();
+            var courses = new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                    Name = "Happy Days",
+                    Provider = new Provider
+                    {
+                        Id = 10,
+                        Name = "The Fonz",
+                    },
+                }
+            };
+            contextMock.Setup(c =>
+                c.GetLocationFilteredCourses(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>())
+            ).Returns(courses.AsQueryable());
             ICourseSearchService courseSearchService = new CourseSearchService(contextMock.Object);
             var filter = new QueryFilter
             {
-                pageSize = 123,
+                lat = 1.23,
+                lng = 2.34,
+                rad = (int)RadiusOption.TenMiles,
             };
             var results = courseSearchService.GetFilteredCourses(filter);
-            results.Count.Should().Be(0, "there's no mock data yet");
+            results.Count.Should().Be(1, "there's one course and location is mocked");
         }
     }
 }
