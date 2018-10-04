083e114405295da45d7e44ea18cd47a4987d11b9
Tim Abell <tim@timwise.co.uk>
2018-10-04 09:39:34 +0100

    Stub CourseSearchService test

---
+++ tests/Unit.Tests/Services/CourseSearchServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using GovUk.Education.SearchAndCompare.Api.DatabaseAccess;
+using GovUk.Education.SearchAndCompare.Api.Services;
+using GovUk.Education.SearchAndCompare.Domain.Filters;
+using Moq;
+using NUnit.Framework;
+
+namespace GovUk.Education.SearchAndCompare.Api.Tests.Unit.Tests.Services
+{
+    [TestFixture]
+    public class CourseSearchServiceTests
+    {
+        [Test]
+        public void CheckFiltering()
+        {
+            var contextMock = new Mock<ICourseDbContext>();
+            ICourseSearchService courseSearchService = new CourseSearchService(contextMock.Object);
+            var filter = new QueryFilter
+            {
+                pageSize = 123,
+            };
+            var results = courseSearchService.GetFilteredCourses(filter);
+            results.Count.Should().Be(0, "there's no mock data yet");
+        }
+    }
+}
