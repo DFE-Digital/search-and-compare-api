8af4849fbdc2758a6a46363d5d03472391a253f1
Tim Abell <tim@timwise.co.uk>
2018-10-05 12:58:07 +0100

    Migration - location_distance pg function now returns campus info

---
+++ src/api/Migrations/20181005113235_CampusDistance.cs
@@ -0,0 +1,43 @@
+﻿using GovUk.Education.SearchAndCompare.Api.Sql;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SearchAndCompare.Migrations
+{
+    public partial class CampusDistance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // drop is required because we are changing the signature of the function
+            // - ERROR: cannot change return type of existing function DETAIL: Row type defined by OUT parameters is different.HINT: Use DROP FUNCTION course_distance(double precision, double precision, double precision) first.
+            migrationBuilder.Sql("drop function course_distance;");
+            migrationBuilder.Sql(FunctionDefinitions.CourseDistance);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop function course_distance;");
+            migrationBuilder.Sql(@"CREATE OR REPLACE FUNCTION course_distance( lat DOUBLE PRECISION, 
+                                                            lon DOUBLE PRECISION, 
+                                                            rad DOUBLE PRECISION) 
+                                            RETURNS TABLE (""Id"" integer,
+                                                           ""Distance"" double precision) AS $$
+                    SELECT ""Id"", MIN(""Distance"") AS ""Distance""
+                    FROM (
+                        SELECT course.""Id"", earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) AS ""Distance""
+                        FROM ""course""
+                        JOIN location loc ON course.""ProviderLocationId"" = loc.""Id""
+                        WHERE ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"")
+                            AND earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) <= rad
+                        UNION ALL
+                        SELECT course.""Id"", earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) AS ""Distance"" 
+                        FROM ""course""
+                        JOIN campus on campus.""CourseId"" = course.""Id""
+                        JOIN location loc ON campus.""LocationId"" = loc.""Id""			
+                        WHERE ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"")
+                            AND earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) <= rad ) x
+                    GROUP BY ""Id""       
+                $$ LANGUAGE SQL;");
+
+        }
+    }
+}
---
+++ src/api/Sql/FunctionDefinitions.cs
@@ -0,0 +1,47 @@
+﻿namespace GovUk.Education.SearchAndCompare.Api.Sql
+{
+    /// <summary>
+    /// A quick hack to make the migrations of replacable sql more source-control friendly.
+    /// If you link your migration code back to these strings you'll be able to see diffs in git as they change
+    /// which you can't with EF migration files.
+    /// </summary>
+    public static class FunctionDefinitions
+    {
+        public const string CourseDistance = @"
+CREATE OR REPLACE
+FUNCTION course_distance( lat DOUBLE PRECISION, lon DOUBLE PRECISION, rad DOUBLE PRECISION) 
+RETURNS TABLE (""CourseId"" integer, ""CampusId"" integer, ""Distance"" double precision)
+AS $$
+	-- Returns course with a provider or campus within the specified radius of the specificied latitude/longitude location.
+	-- If the match was a provider then CampusId is null, if it was a campus then CampusId will be the nearest campus for that course.
+	-- CourseIds will be unique in the returned table
+	-- Usage: select * FROM location_distance(51.5073509,-0.1277583,8046.7)
+	SELECT distinct on (""CourseId"") -- only take nearest entry for each course https://www.postgresql.org/docs/current/static/sql-select.html#SQL-DISTINCT
+		""CourseId"", ""CampusId"", ""Distance""
+	FROM
+	(
+		(
+			-- find by provider location
+			SELECT course.""Id"" ""CourseId"", earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) AS ""Distance"", null as ""CampusId""
+			FROM ""course""
+				JOIN location loc ON course.""ProviderLocationId"" = loc.""Id""
+			WHERE ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"")
+				AND earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) <= rad
+		)
+		-- A UNION is used rather than a JOIN as it was found to be an order of magnitude faster.
+		UNION ALL
+		(
+			-- find by nearest campus location for each course
+			SELECT course.""Id"" ""CourseId"", earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) AS ""Distance"", campus.""Id"" as ""CampusId""
+			FROM ""course""
+				JOIN campus on campus.""CourseId"" = course.""Id""
+				JOIN location loc ON campus.""LocationId"" = loc.""Id""            
+			WHERE ""earth_box""(ll_to_earth(lat, lon), rad) @> ll_to_earth(loc.""Latitude"",loc.""Longitude"")
+				AND earth_distance(ll_to_earth(lat, lon), ll_to_earth(loc.""Latitude"",loc.""Longitude"")) <= rad
+	  	)
+	) as subquery -- alias is required but unused
+	order by ""CourseId"", ""Distance"" -- has to be sorted by CourseId first for ""distinct on"" to work
+$$ LANGUAGE SQL;
+";
+    }
+}
