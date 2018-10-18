88fc507fd1c87823391637ea64a380367bdd4962
Martin Bentley <martinbentley@btinternet.com>
2018-10-18 13:22:57 +0100

    Added migrations

--- src/api/Migrations/20181018120742_[VacStatus].Designer.cs
+++ src/api/Migrations/20181018120742_[VacStatus].Designer.cs
@@ -3,15 +3,17 @@ using System;
 using GovUk.Education.SearchAndCompare.Api.DatabaseAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace SearchAndCompare.Migrations
 {
     [DbContext(typeof(CourseDbContext))]
-    partial class CourseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181018120742_[VacStatus]")]
+    partial class VacStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
