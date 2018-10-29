a5b6c22c8a5e71afbe40fb48bc287cf5d868c252
Martin Bentley <martinbentley@btinternet.com>
2018-10-29 14:19:19 +0000

    Encrypted nuget source

--- travis-nuget-deploy.sh
+++ travis-nuget-deploy.sh
@@ -1,4 +1,5 @@
 ApiKey=$1
+Source=$2
 
 dotnet pack ./src/domain
-dotnet nuget push ./src/domain/bin/**/*.nupkg -k $ApiKey -s https://www.nuget.org || echo "Nuget deploy skipped"
+dotnet nuget push ./src/domain/bin/**/*.nupkg -k $ApiKey -s $Source || echo "Nuget deploy skipped"
