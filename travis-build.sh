d0c61bc64183f502ddd0f019e280160c37307c55
Martin Bentley <martinbentley@btinternet.com>
2018-10-29 11:27:04 +0000

    Changed proj file

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
-dotnet restore ./src/api/SearchAndCompareApi.csproj -v m
-dotnet restore ./src/domain/SearchAndCompareDomain.csproj -v m
-dotnet restore ./tests/SearchAndCompareApi.Tests.csproj -v m
+dotnet restore ./src/api/SearchAndCompareApi.csproj -v q
+dotnet restore ./src/domain/SearchAndCompareDomain.csproj -v q
+dotnet restore ./tests/SearchAndCompareApi.Tests.csproj -v q
 
 dotnet test ./tests/SearchAndCompareApi.Tests.csproj
 dotnet test ./tests/SearchAndCompareApi.Tests.csproj --filter TestCategory="Integration"
