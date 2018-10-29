dbf33edd5eeb925495359d247d28f46fb2b35b4f
Martin Bentley <martinbentley@btinternet.com>
2018-10-29 11:24:23 +0000

    Fix attempt for source error

--- travis-nuget-deploy.sh
+++ travis-nuget-deploy.sh
@@ -1,5 +1,7 @@
 ApiKey=$1
 Source=$2
 
+echo $Source
+echo "Source"
 dotnet pack ./src/domain
-dotnet nuget push ./src/Domain/bin/**/*.nupkg -k $ApiKey -s $Source || echo "Nuget deploy skipped"
+dotnet nuget push ./src/Domain/bin/**/*.nupkg -k $ApiKey -s https://www.nuget.org || echo "Nuget deploy skipped"
