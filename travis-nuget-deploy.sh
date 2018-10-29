16cb04d561c41951620fdfa394c3c16a0d3bda5f
Martin Bentley <martinbentley@btinternet.com>
2018-10-29 12:29:38 +0000

    Added encrypted nuget key

--- travis-nuget-deploy.sh
+++ travis-nuget-deploy.sh
@@ -1,5 +1,4 @@
 ApiKey=$1
-ls ./src/domain
-ls ./src/Domain/bin/**/*.nupkg
+
 dotnet pack ./src/domain
 dotnet nuget push ./src/Domain/bin/**/*.nupkg -k $ApiKey -s https://www.nuget.org || echo "Nuget deploy skipped"
