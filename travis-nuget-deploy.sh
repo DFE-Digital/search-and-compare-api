25a35c67cd9be2f1c212cf47b9fbd1905841f85a
Martin Bentley <martinbentley@btinternet.com>
2018-10-29 11:35:26 +0000

    changes to yml

--- travis-nuget-deploy.sh
+++ travis-nuget-deploy.sh
@@ -1,7 +1,5 @@
 ApiKey=$1
-Source=$2
-
-echo $Source
-echo "Source"
+ls ./src/domain
+ls ./src/Domain/bin/**/*.nupkg
 dotnet pack ./src/domain
 dotnet nuget push ./src/Domain/bin/**/*.nupkg -k $ApiKey -s https://www.nuget.org || echo "Nuget deploy skipped"
