fe4e32eb80246ebf39cbd171656a865b0151d507
De Fong <de_fong2k@yahoo.com>
2018-12-12 13:34:36 +0000

    Added additional logging Added telemetry to denote error for failures for failed geo coding Fixed the geo coder to be take in account lack of LastGeocodedUtc

--- tests/Integration.Tests/Geocoder/LocationRequester.Tests.cs
+++ tests/Integration.Tests/Geocoder/LocationRequester.Tests.cs
@@ -15,6 +15,7 @@ using MockQueryable.Moq;
 using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using FluentAssertions;
 
 namespace GovUk.Education.SearchAndCompare.Api.Tests.Integration.Tests.Geocoder
 {
@@ -37,6 +38,81 @@ namespace GovUk.Education.SearchAndCompare.Api.Tests.Integration.Tests.Geocoder
             context.Locations.RemoveRange(context.Locations);
             context.SaveChanges();
         }
+
+
+        [Test]
+        public void CompleteTest()
+        {
+            var now = DateTime.Now;
+            var testLocations = new List<Location>();
+            var geocodedLocations = Enumerable.Range(1, 10).ToList().Select(x => new Location {GeoAddress = x.ToString(), Address = x.ToString(), Latitude = x, Longitude = x, LastGeocodedUtc = now.AddDays(-x)});
+            var tobeGeocoded = Enumerable.Range(1, 10).ToList().Select(x => {
+                var location = new Location() {GeoAddress= "Needs_to_be_gecoded_" + x, Address = x.ToString()};
+
+                var mod = x % 4;
+                switch(mod)
+                {
+                    case 0:
+                        location.Latitude = x;
+                        location.LastGeocodedUtc = now;
+                        break;
+                    case 1:
+                        location.Longitude = x;
+                        location.LastGeocodedUtc = now;
+                        break;
+                    case 2:
+                        location.LastGeocodedUtc = now;
+                        break;
+                    case 3:
+                        location.Latitude = x;
+                        location.Longitude = x;
+                        location.LastGeocodedUtc = DateTime.MinValue;
+                        break;
+                }
+                return location;
+                });
+
+            var httpClientSetup = tobeGeocoded.ToDictionary(x => "https://maps.googleapis.com/maps/api/geocode/json?key=apiKey&region=uk&address=" + x.GeoAddress, x => ResponseWithBody(new {
+                    status = "OK",
+                    results = new object[] {
+                        new {
+                            address_components = new object[] {
+                                new {
+                                    types = new string[] {
+                                        "country"
+                                    },
+                                    short_name = "GB"
+                                }
+                            },
+                            formatted_address = x.GeoAddress.Replace('_', ' '),
+                            geometry = new {
+                                location = new {
+                                    lat = double.Parse(x.Address) * 2,
+                                    lng = double.Parse(x.Address) * 2
+                                }
+                            }
+                        }
+                    }}));
+
+            foreach (var item in httpClientSetup)
+            {
+                httpClient.Setup(x => x.GetAsync(item.Key))
+                    .ReturnsAsync(item.Value)
+                    .Verifiable();
+            }
+
+            context.Locations.AddRange(geocodedLocations);
+            context.Locations.AddRange(tobeGeocoded);
+            context.SaveChanges();
+
+            system.RequestLocations().Wait();
+
+            httpClient.VerifyAll();
+            var hasGeocoded = context.Locations.Where(x => x.GeoAddress.StartsWith("Needs_to_be_gecoded_")).ToList();
+            hasGeocoded.Should().HaveCount(tobeGeocoded.Count());
+            hasGeocoded.All(x => x.Latitude == double.Parse(x.Address) * 2 && x.Longitude == double.Parse(x.Address) * 2 && x.FormattedAddress == x.GeoAddress.Replace('_', ' ')).Should().BeTrue();
+        }
+
 
         [Test]
         public void WithAResult()
@@ -68,18 +144,18 @@ namespace GovUk.Education.SearchAndCompare.Api.Tests.Integration.Tests.Geocoder
                 }))
                 .Verifiable();
 
-            context.Locations.Add(new Location{ GeoAddress = "FakeStreet", Address = "old address that will no longer get geo coded" });
+            context.Locations.Add(new Location{ GeoAddress = "FakeStreet", Address = "old address that will  get geo coded" });
             context.SaveChanges();
 
             system.RequestLocations().Wait();
 
             httpClient.VerifyAll();
             var res = context.Locations.Single();
-            Assert.AreEqual("FakeStreet", res.GeoAddress);
-            Assert.AreEqual("old address that will no longer get geo coded", res.Address);
-            Assert.AreEqual("Formatted fake street", res.FormattedAddress);
-            Assert.AreEqual(12.3, res.Latitude);
-            Assert.AreEqual(23.1, res.Longitude);
+            res.GeoAddress.Should().Be("FakeStreet");
+            res.Address.Should().Be("old address that will no longer get geo coded");
+            res.FormattedAddress.Should().Be("Formatted fake street");
+            res.Latitude.Should().Be(12.3);
+            res.Longitude.Should().Be(23.1);
 
             Assert.LessOrEqual(startTime, res.LastGeocodedUtc);
             Assert.GreaterOrEqual(DateTime.UtcNow, res.LastGeocodedUtc);
@@ -91,7 +167,7 @@ namespace GovUk.Education.SearchAndCompare.Api.Tests.Integration.Tests.Geocoder
             httpClient.Setup(x => x.GetAsync(It.IsAny<string>()))
                 .ThrowsAsync(new Exception("shouldn't be called"));
 
-            context.Locations.Add(new Location { Address = "FakeStreet", FormattedAddress = "Formatted fake street", Latitude = 12.3, Longitude = 23.1});
+            context.Locations.Add(new Location { GeoAddress = "GeoAddress", Address = "FakeStreet", FormattedAddress = "Formatted fake street", Latitude = 12.3, Longitude = 23.1, LastGeocodedUtc = DateTime.Now});
             context.SaveChanges();
 
             Assert.DoesNotThrowAsync(() => system.RequestLocations());
