9fcbb766ce3690aec86f602ee5d306dc58313c36
Martin Bentley <martinbentley@btinternet.com>
2018-12-13 16:16:40 +0000

    Added migration to clear orphaned records

---
+++ src/api/Migrations/20181213160201_ClearOrphanedCampusRecords.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SearchAndCompare.Migrations
+{
+    public partial class ClearOrphanedCampusRecords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from campus where \"CourseId\" is null;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //do nuffink
+        }
+    }
+}
