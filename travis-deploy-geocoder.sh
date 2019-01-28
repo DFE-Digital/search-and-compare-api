bd2a03955e7a8cde3e566249472852ac8f23b1ec
Theodor Vararu <theo@vararu.org>
2019-01-28 18:09:19 +0000

    Fix geocoder webjob deploy

--- travis-deploy-geocoder.sh
+++ travis-deploy-geocoder.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
+
+echo "Deploying geocoder..."
+
+# script for deploying webjobs to any environment, choosing the matching deploy password environment variable
+
+# arg 1: environment to deploy to
+env=$1
+
+# convert env to uppercase to match environment variable case
+upperenv=`echo "$env" | tr '[:lower:]' '[:upper:]'`
+
+eval "password=\$APP_CREDENTIALS_${upperenv}_PWD"
 
 deployZip=deploy.zip
 
@@ -12,6 +25,7 @@ echo "creating zip..."
 zip $deployZip -r src/geocoder/bin/Release/netcoreapp2.1/publish
 
 echo "uploading to azure WebJob..."
-curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/geocoder/
+curl -X PUT -u "\$bat-$env-search-and-compare-api-app:$password" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://bat-$env-search-and-compare-api-app.scm.azurewebsites.net/api/triggeredwebjobs/geocoder/
+
 
 echo "Deploy complete."
